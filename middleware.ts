e8fa68cd7ab3dc810f81a1ea2c6d2ab2b54103cc
alexdoffgit <alexdoffacc@gmail.com>
2023-04-17 20:39:46 +0700

    update middleware to include protected url

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,25 @@
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
-import type { Database } from '@utils/database.types'
+import type { NextRequest } from "next/server";
+import type { Database } from "@utils/database.types";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareSupabaseClient<Database>({ req, res })
-  await supabase.auth.getSession()
-  return res
+  const protectedRoutes = ['/editor', '/settings']
+  
+  const res = NextResponse.next();
+  const supabase = createMiddlewareSupabaseClient<Database>({ req, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  for (let route of protectedRoutes) {
+    if(!session && req.nextUrl.pathname.startsWith(route)) {
+      const redirectUrl = req.nextUrl.clone();
+      redirectUrl.pathname = '/';
+      return NextResponse.redirect(redirectUrl)
+    }
+  }
+
+  return res;
 }
