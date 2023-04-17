0c157e99ebaae3acfb7bad55d9644c5896b386b3
alexdoffgit <alexdoffacc@gmail.com>
2023-04-17 20:42:56 +0700

    add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,9 @@
-import { FormContainer } from './form'
+import { FormContainer } from "./form";
 
 export default function Login() {
-    return (
-        <div className="w-full">
-            <div className="grid grid-cols-6">
-                <FormContainer />
-            </div>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-6">
+      <FormContainer />
+    </div>
+  );
 }
