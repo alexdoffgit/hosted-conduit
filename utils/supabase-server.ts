5ff114667041c118a05e158fbbe61b472d13cf9b
alexdoffgit <alexdoffacc@gmail.com>
2023-05-08 14:08:19 +0700

    update supabase server client to be server only

--- utils/supabase-server.ts
+++ utils/supabase-server.ts
@@ -1,3 +1,4 @@
+import "server-only"
 import { headers, cookies } from "next/headers";
 import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "./database.types";
