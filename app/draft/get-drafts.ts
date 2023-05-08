1cd48ca5fb36ee793ad8e23c35f0171cbd65cb5f
alexdoffgit <alexdoffacc@gmail.com>
2023-05-08 14:23:06 +0700

    add display drafts page
    this is the feature that will display draft article based on current logged in user

---
+++ app/draft/get-drafts.ts
@@ -0,0 +1,29 @@
+import { getSessionId } from "@utils/session-id";
+import { createServerClient } from "@utils/supabase-server";
+import "server-only";
+
+export async function getDrafts() {
+    const supabase = createServerClient()
+    const {sessionId, error} = await getSessionId()
+
+    if(sessionId) {
+        let { data, error } = await supabase.from('articles').select('id,title,description').eq('published', false).eq('author_id', sessionId)
+
+        if(error) {
+            return {
+                articles: null,
+                error
+            }
+        }
+
+        return {
+            articles: data,
+            error
+        }
+    }
+
+    return {
+        articles: null,
+        error
+    }
+}
